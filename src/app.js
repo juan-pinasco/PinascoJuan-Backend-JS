0fa932fd9d1704a328e3614d1aa1efce39c0f988
Juan Pinasco <juanpinascoprogramacion@gmail.com>
2023-08-07 19:31:19 -0300

    0.1

--- src/app.js
+++ src/app.js
@@ -1,17 +1,43 @@
 import express from "express";
 import productsRouter from "./routes/products.router.js";
 import cartsRouter from "./routes/carts.router.js";
+//
+import { __dirname } from "./utils.js";
+import handlebars from "express-handlebars";
+import viewsRouter from "./routes/views.router.js";
+import { Server } from "socket.io";
 
 const app = express();
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
+
+//
+app.use(express.static(__dirname + "/public"));
+app.engine("handlebars", handlebars.engine());
+app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars");
+app.use("/api/views", viewsRouter);
 
 //rutas
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
 
-app.listen(8080, () => {
+/* app.listen(8080, () => {
   console.log("escuchando al puerto 8080");
+}); */
+
+//
+const PORT = 8080;
+const httpServer = app.listen(PORT, () => {
+  console.log(`escuchando al puerto ${PORT}`);
+});
+
+const socketServer = new Server(httpServer);
+
+socketServer.on("connection", (socket) => {
+  console.log(`Cliente conectado: ${socket.id}`);
+  socket.on("disconnect", () => {
+    console.log(`Cliente desconectado: ${socket.id}`);
+  });
 });
